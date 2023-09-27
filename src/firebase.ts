d41e484e3984232e8e0ad7ad4d0be7e536cbadbb
Sean <sean.preston2904@gmail.com>
2023-09-27 13:14:19 +1300

    Updated Firebase Config Again

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,11 +5,6 @@ import { getFirestore, query, getDocs, collection, where, addDoc} from "firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
 const firebaseConfig = {
     apiKey: "AIzaSyBzJ8ulbByPwIB4HwL000XH7d_VszLTKQc",
     authDomain: "gain-90633.firebaseapp.com",
@@ -18,6 +13,11 @@ const firebaseConfig = {
     messagingSenderId: "214143056499",
     appId: "1:214143056499:web:d67bbe5207adfd56f50806"
 }
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 // Google Authentication
 const googleProvider = new GoogleAuthProvider();
